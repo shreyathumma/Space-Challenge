8548a5604163e8ff4d1ae9bc1b96b80a54ab7009
shreyathumma <shreyathumma@yahoo.com>
2018-04-03 07:45:05 -0500

    Update Rocket.java

--- src/Rocket.java
+++ src/Rocket.java
@@ -1,13 +1,13 @@
-
-
 public class Rocket implements Spaceship {
 
+	private int rocketCost;
 	private int rocketWeight;
 	private int maxRocketWeight;
 	private int maxCargo;
-	private int currentWeight;
+	private int cargoWeight;
 	private int totalWeight;
 	
+	private static int num_trials;
 	
 	public boolean launch(){
 		return true;
@@ -17,29 +17,67 @@ public class Rocket implements Spaceship {
 		return true;
 	}
 	
+	public void setRocketCost(int cost){
+		rocketCost = cost;
+	}
 	
-	public boolean canCarry(Item itemobject){
-		
-		if(currentWeight <= maxRocketWeight){
+	public int getRocketCost(){
+		return rocketCost;
+	}
+	
+	public void setRocketWeight(int weight){
+		rocketWeight = weight;
+	}
+	
+	public void setMaxRocketWeight(int weight){
+		maxRocketWeight = weight;
+	}
+	
+	public int getMaxRocketWeight(){
+		return maxRocketWeight;
+	}
+	
+	public void setMaxCargo(){
+		maxCargo = maxRocketWeight - rocketWeight;
+	}
+	
+	public int getMaxCargo(){
+		return maxCargo;
+	}
+	
+	public void setCargoWeight(Item item){
+		cargoWeight += item.getWeight();
+	}
+	
+	public int getCargoWeight(){
+		return cargoWeight;
+	}
+
+	public int getTotalWeight(){
+		totalWeight = rocketWeight + getCargoWeight();
+		return totalWeight;
+	}
+	
+
+	public static void setNumberTrials(){
+		num_trials++;
+	}
+	
+	public static int getNumberTrials(){
+		return num_trials;
+	}
+	
+	public boolean canCarry(Item item){
+		if(getCargoWeight() <= getMaxCargo() && getTotalWeight() <= getMaxRocketWeight()){
 			return true;
 		}
 		else{
+			System.out.println("Overloaded UOne Rocket");
 			return false;
 		}
 	}
 	
-	public void setCurrentWeight(Item itemobject){
-		currentWeight += itemobject.getItemWeight();
-	}
-	
-	public int getCurrentWeight(){
-		return currentWeight;
-	}
-
-	public int getTotalWeight(){
-		return totalWeight = rocketWeight + getCurrentWeight();
-	}
-	public void carry(Item itemobject){
-		setCurrentWeight(itemobject);
+	public void carry(Item item){
+		setCargoWeight(item);
 	}
 }
